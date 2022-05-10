c5efe9faf902ca52f0e20ae75038ac178794a743
Jishkarianii <jishkarianigigi9@gmail.com>
2022-05-10 17:37:44 +0400

    Project is done!

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Analytics from './pages/Analytics';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-
-// from 1980 to now: https://api.worldbank.org/v2/country/ge/indicator/NY.GDP.MKTP.CD?date=1980:2022
-
-// from 1980 to now (All): https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?date=1980:2022
-
-// from 1980 to now (JSON): https://api.worldbank.org/v2/country/ge/indicator/NY.GDP.MKTP.CD?date=1980:2022&format=json
-
-
-
-// for chart: https://data.worldbank.org/indicator/NY.GDP.MKTP.CD
-
-
-const api = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?&format=json&date=2020"
-
 
 function App() {
   return (
